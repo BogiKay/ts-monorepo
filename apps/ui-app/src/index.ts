35d2a240a6f9580b0ff7cef74e74fbbe1cd1eab7
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-11-20 14:00:33 +0100

    clean up

--- apps/ui-app/src/index.ts
+++ apps/ui-app/src/index.ts
@@ -1,8 +1,10 @@
 import { isEven } from '@monorepo/utils';
+import moment from 'moment';
 
 const Test = () => {
   const test = Math.floor(Math.random() * 5);
-  console.log(`${test} is even: ${isEven(test)}`);
+  console.log(`[${moment().toISOString()}] FooComponent: \
+  ${test} -> isEven: ${isEven(test)}`);
 
   return {};
 };
